43aef6e147c4bc512da00f759b43d5127be7573d
McIlroy, Stephanie A <stephanie.a.mcilroy@accenture.com>
2020-03-28 10:38:40 -0400

    updating call based on query params

--- script.js
+++ script.js
@@ -1,55 +1,89 @@
-$(function(){
+$(function () {
   const $ringImages = $('.ring-images');
-  const $filterCheckboxes = $('input[type="checkbox"]'); 
+  const $filterCheckboxes = $('input[type="checkbox"]');
   const $filterLinks = $('.ring-price');
   const selectedFilters = {};
   const dataFilters = {}
+  const urlParams = new URLSearchParams(window.location.search);
+  const urlParamsString = urlParams.toString();
+  console.log($filterCheckboxes)
 
+
+
+  // filter(':checked')
   $.ajax({
     type: 'GET',
     url: 'https://quiet-reef-23667.herokuapp.com/rings',
-    success: function(rings) {
-      $.each(rings, function(i, ring){
+    data: urlParamsString,
+    success: function (rings) {
+      var input = document.querySelectorAll('input');
+      $.each(rings, function (i, ring) {
+
+        urlParams.forEach(function (value, name) {
+          input.forEach(function (item) {
+            if (item.value === value) {
+              item.setAttribute('checked', true);
+
+            }
+
+          })
+
+        })
+
         $ringImages.append(`
         <li class="ring-image" data-id="${ring.title}" data-category="${ring.gender} ${ring.material} ${ring.stoneType}">
           <img src=${ring.img}>
           ${ring.title}
         </li>
-        `) 
+        `)
       })
+      updateCheckboxes();
     },
-    error: function(){
+    error: function () {
       console.log('error loading images')
     }
   });
 
 
-  $($filterCheckboxes, $filterLinks).on('change', function(e) {
-    $filterCheckboxes.filter(':checked').each(function() {
+  $($filterCheckboxes, $filterLinks).on('change', function (e) {
+    updateCheckboxes()
+
+  })
+
+  function updateCheckboxes() {
+    $filterCheckboxes.filter(':checked').each(function () {
       if (!selectedFilters.hasOwnProperty(this.name)) {
         selectedFilters[this.name] = [];
       }
       selectedFilters[this.name].push(this.value);
-      console.log(this.name, this.value)
-      console.log(selectedFilters, "from checkbox")
-      dataFilters[this.name] = this.value; 
-      
+
+      dataFilters[this.name] = this.value;
+      console.log('dataFilters', dataFilters)
+      // getNewItems(dataFilters)
     });
-      console.log('clicked')
+
+  }
+  // 
+
+
+
+  function getNewItems(dataFilters) {
+    // if (dataFilters[])
+
     $.ajax({
       type: 'GET',
       url: `https://quiet-reef-23667.herokuapp.com/rings?`,
       data: dataFilters,
-      success: function() {
+      success: function () {
         let url = this.url;
-        let newUrl = url.substring(url.lastIndexOf("?") + 1);
-        window.location.hash = newUrl
+        let newUrl = url.substring(url.lastIndexOf("?"));
+        window.location.search = newUrl
         let $filteredResults = $('.ring-image');
-        $.each(selectedFilters, function(i, filterValues) {
-          $filteredResults = $filteredResults.filter(function() {
+        $.each(selectedFilters, function (i, filterValues) {
+          $filteredResults = $filteredResults.filter(function () {
             let matched = false,
               currentFilterValues = $(this).data('category').split(' ');
-            $.each(currentFilterValues, function(i, currentFilterValue) {
+            $.each(currentFilterValues, function (i, currentFilterValue) {
               if ($.inArray(currentFilterValue, filterValues) !== -1) {
                 matched = true;
               }
@@ -59,15 +93,15 @@ $(function(){
         });
         $('.ring-image').hide().filter($filteredResults).show();
       },
-      error: function(){
+      error: function () {
         console.log('error loading images')
       }
     });
 
-  });
+  }
+
 
 })
-
 
           // for(let [key, value] of Object.entries(dataFilters)){
           //   let refinementCategory = key;
@@ -96,7 +130,7 @@ $(function(){
 //       console.log('error loading images')
 //     }
 //   });
-  
+
 //   $filterCheckboxes.on('change', function() {
 //     const selectedFilters = {};
 //     const dataFilters = {}
